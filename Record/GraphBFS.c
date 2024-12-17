117eaa63c185193776717533fd7619f02ebc3754
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2024-12-17 10:00:23 +0530

    Update GraphBFS.c

--- Record/GraphBFS.c
+++ Record/GraphBFS.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void BFS(int graph[10][10], int start, int n) {
-    int visited[10] = {0};
-    int queue[10];
-    int fr = 0, re = -1;
+void BFS(int graph[10][10], int start, int n)
+{
+    int visited[10] = {0}, queue[10], fr = 0, re = -1;
     visited[start] = 1;
     queue[++re] = start;
-
-    while (fr <= re) {
-        int curr = queue[fr++];
+    while (fr <= re)
+    {
+        int curr = queue[fr++];  //dequeue
         printf("%d ", curr);
-        for (int i = 0; i < n; i++) {
-            if (graph[curr][i] == 1 && !visited[i]) {
+        for (int i = 0; i < n; i++)
+            if (graph[curr][i] && !visited[i])
+            {
                 visited[i] = 1;
-                queue[++re] = i;
+                queue[++re] = i;  //enqueue
             }
-        }
     }
 }
 
-void main() {
+void main()
+{
     int n, graph[10][10];
-
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-
     printf("Enter the adjacency matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             scanf("%d", &graph[i][j]);
-        }
-    }
     printf("Enter the starting vertex: ");
     int start;
     scanf("%d", &start);
