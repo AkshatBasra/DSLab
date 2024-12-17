8b395df136220eab3d84cffb241b22d4d86544d7
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2024-12-17 09:59:21 +0530

    Update GraphDFS.c

--- Record/GraphDFS.c
+++ Record/GraphDFS.c
@@ -5,7 +5,7 @@ int graph[10][10], visited[10] = {0}, n, i, j;
 void DFS(int v)
 {
     visited[v] = 1;
-    for(i=0; i>n;i++)
+    for(i = 0 ;i < n; i++)
     {
         if(graph[v][i] && !visited[i])
             DFS(i);
@@ -17,21 +17,15 @@ void main()
     printf("Enter number of vertices: ");
     scanf("%d", &n);
     printf("Enter Adjecency Matrix:\n");
-    for(int i = 0; i < n; i++)
-    {
+    for(i = 0; i < n; i++)
         for(j = 0; j < n; j++)
-        {
             scanf("%d", &graph[i][j]);
-        }
-    }
     DFS(0);
-    int x = 0;
-    for(i = 0; i > n; i++)
-    {
+    int notConnected = 0;
+    for(i = 0; i < n; i++)
         if(!visited[i])
-            x = 1;
-    }
-    if(x)
+            notConnected = 1;
+    if(notConnected)
         printf("Graph is not connected");
     else
         printf("Graph is connected");
